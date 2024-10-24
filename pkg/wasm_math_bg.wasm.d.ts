1ee6350e51d6525e306775b3ea89e53a7b446772
tetsutooo <john.tetsuto0@gmail.com>
2024-10-24 19:09:31 +0900

    Add files via upload

---
+++ pkg/wasm_math_bg.wasm.d.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_system_free(a: number, b: number): void;
+export function system_new(a: number, b: number, c: number): number;
+export function system_update(a: number): void;
+export function system_get_normalized_z(a: number, b: number): void;
+export function system_get_size_distribution(a: number, b: number): void;
+export function system_reset_size_distribution(a: number): void;
+export function __wbg_rational64_free(a: number, b: number): void;
+export function rational64_new(a: number, b: number): number;
+export function rational64_to_f32(a: number): number;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number, c: number): void;
