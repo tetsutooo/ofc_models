d19dc1cbdf79942530d5372d37c3651d44e2777b
tetsutooo <john.tetsuto0@gmail.com>
2024-12-05 11:59:13 +0900

    Add files via upload

---
+++ pkg/game_of_life_bg.wasm.d.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_gol_free: (a: number, b: number) => void;
+export const gol_new: (a: number, b: number, c: number, d: number) => number;
+export const gol_init: (a: number) => void;
+export const gol_generate: (a: number) => void;
+export const gol_display: (a: number, b: any) => void;
+export const gol_draw_graph: (a: number, b: any, c: number) => void;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_start: () => void;
